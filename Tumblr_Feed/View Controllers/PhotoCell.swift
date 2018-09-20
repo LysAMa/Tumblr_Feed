d4053827f5e60c6c2c65f5e83181a393d7ffb7d2
LysAMa <ladouceurlyse44@gmail.com>
2018-09-20 18:32:31 -0400

    lab 1

---
+++ Tumblr_Feed/View Controllers/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  Tumblr_Feed
+//
+//  Created by MacBookPro10 on 9/20/18.
+//  Copyright © 2018 MacBookPro10. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var imagefeed: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
